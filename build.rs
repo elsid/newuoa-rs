e59de2c2f1773dc9cbd0a510f0d5e12e54237315
elsid <elsid.mail@gmail.com>
2015-09-06 21:49:41 +0300

    Use absolute paths in build script

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() {
+    use std::path::Path;
     use std::process::Command;
-    Command::new("cmake").args(&["."]).current_dir("newuoa-cpp").status().unwrap();
-    Command::new("make").current_dir("newuoa-cpp").status().unwrap();
-    println!("cargo:rustc-link-search=native=newuoa-cpp/lib");
+    let dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("newuoa-cpp");
+    Command::new("cmake").args(&["."]).current_dir(&dir).status().unwrap();
+    Command::new("make").current_dir(&dir).status().unwrap();
+    println!("cargo:rustc-link-search=native={}", dir.join("lib").display());
     println!("cargo:rustc-link-lib=static=newuoa");
 }
